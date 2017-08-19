907a4e29feb9a15619092774ebb9cff1e395b17c
VladoGoIt <dzhuhalyk@gmail.com>
2017-08-19 05:26:42 +0300

    created method sale

--- src/flowers/FlowerStore.java
+++ src/flowers/FlowerStore.java
@@ -4,4 +4,7 @@ package flowers;
  * Created by Vlado on 18.08.2017.
  */
 public class FlowerStore {
+    public Flowers {} flowersList sale(int roseQuan, int romashkaQuan, int tulipQuan){
+
+    }
 }
